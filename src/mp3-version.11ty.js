32aeacdb535a9d3a2e8f2a4089ef5fc72af12784
Larry Hudson <larryhudson@hey.com>
2022-07-17 09:09:46 +1000

    HTML to text formatting

--- src/mp3-version.11ty.js
+++ src/mp3-version.11ty.js
@@ -1,3 +1,28 @@
+const htmlToText = require("html-to-text");
 const { AudioVersionTemplate } = require("eleventy-plugin-text-to-speech");
 
-module.exports = AudioVersionTemplate;
+class AudioArticleTemplate extends AudioVersionTemplate {
+  convertContentToPlainText = async (content) => {
+    return htmlToText.convert(content, {
+      wordwrap: 0,
+      selectors: [
+        {
+          selector: "a",
+          options: {
+            ignoreHref: true,
+          },
+        },
+        {
+          selector: "figure",
+          format: "skip",
+        },
+        {
+          selector: "img",
+          format: "skip",
+        },
+      ],
+    });
+  };
+}
+
+module.exports = AudioArticleTemplate;
